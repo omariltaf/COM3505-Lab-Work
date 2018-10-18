8a808064572612d1e75db385facefe3417d7a54e
Omar <omariltaf@hotmail.co.uk>
2018-10-18 21:11:03 +0100

    Added code for Week 4

--- MyThing/Week04.ino
+++ MyThing/Week04.ino
@@ -8,38 +8,28 @@
 
 const char* ssid = "omarneville";
 const char* password = "password";
+const char HTML[] = "<form onSubmit=\"event.preventDefault()\"><label class=\"label\">Network Name</label><input type=\"text\" name=\"ssid\"/><br/><label>Password</label><input type=\"text\" name=\"pass\"/><br/><input type=\"submit\" value=\"Submit\"></form>";
 
 ESPWebServer webServer(80);
 
 void handleRoot() {
   digitalWrite(LED_BUILTIN, 1);
-  webServer.send(200, "text/html", "hahhahha!");
+  webServer.send(200, "text/html", HTML);
   digitalWrite(LED_BUILTIN, 0);
 }
 
 void setup04() {
   Serial.begin(115200);
-
+  
+  webServer.on("/", handleRoot);
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(ssid, password);
-  Serial.println(WiFi.softAPIP());
   
-
-  //Print the server name and password and local IP
+  Serial.println(WiFi.softAPIP());              // Prints out the IP Address, SSID, and password for the access point
+  //  Serial.println(ssid);
+  //  Serial.println(password);
   
-  Serial.println(ssid);
-  Serial.println(password);
-  Serial.println(WiFi.localIP());
-
-  webServer.on("/", handleRoot);
-
-  webServer.begin();
-  Serial.println("HTTP server started");
-  Serial.println("<!DOCTYPE html><html>");
-  Serial.println("<body><h1>ESP32 Web Server</h1>");
-  Serial.println("</body></html>");
-
-
+  webServer.begin();  
 }
 
 void loop04() {
